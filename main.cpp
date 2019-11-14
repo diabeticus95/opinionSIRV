5ee4b1e822a9ca7e33ec50c17fc0c16a0bdced32
pkowalski <pkowalski>
2019-11-14 13:41:53 +0100

    naming convention

--- main.cpp
+++ main.cpp
@@ -26,9 +26,9 @@ int main() {
 	int cutoff = 50;
 	std::mt19937 mt(time(0)); std::uniform_int_distribution<int> seeds(0, RAND_MAX);
 	time_t begin = clock();
-	run(size, p, cutoff, mt, seeds, std::string("chart_var6_r01_w1"), (double)1/11, (double)10/11);
-	run(size, p, cutoff, mt, seeds, std::string("chart_var6_r10_w1"), (double)10/11, (double)1/11);
-	run(size, p, cutoff, mt, seeds, std::string("chart_var6_r1_w1"), (double)1/2, (double)1/2);
+	run(size, p, cutoff, mt, seeds, std::string("chart_var4_r01_w1"), (double)1/11, (double)10/11);
+	run(size, p, cutoff, mt, seeds, std::string("chart_var4_r10_w1"), (double)10/11, (double)1/11);
+	run(size, p, cutoff, mt, seeds, std::string("chart_var4_r1_w1"), (double)1/2, (double)1/2);
 
 
 
