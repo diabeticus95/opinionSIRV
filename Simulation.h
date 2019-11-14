05141a52dbd0b9d20b3855d3fd93865688d1e6d1
Patryk Kowalski <diabeticus95@gmail.com>
2019-11-14 12:07:35 +0100

    further running tweaks

--- Simulation.h
+++ Simulation.h
@@ -31,7 +31,7 @@ private:
 	std::uniform_int_distribution<int>* neighbor_dist;
 	int dying_period = 6;
 	double b; // bheta getting sick coefficient
-	double w = 0.99; // szczepionka skojarzona MMR przeciw odrze, œwince i ró¿yczce
+	double w = 1;
 	double z; // random antivax events
 	double p; // extremization coefficient;
 	double q; // moderation coefficient
